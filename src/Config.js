598b8d3847d9dfec886286e2c5fe943c9b5a64f2
Anthony Schott <acschott526@gmail.com>
2021-02-13 17:03:02 -0800

    Update text

--- src/Config.js
+++ src/Config.js
@@ -10,8 +10,10 @@ export const colors = {
 export const content = {
   mainTitle: "Natalia Ong's Portfolio",
   subtitle: "Concept Art & Illustration",
-  aboutMe: "asdf",
-  lolbyte: "qwer",
+  aboutMe:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  lolbyte:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 };
 
 export const links = {
