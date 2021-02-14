1368ffc7b3e086168decbd522b14c7b6035f2660
Anthony Schott <acschott526@gmail.com>
2021-02-13 16:53:43 -0800

    Add favicon and download link for resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,11 +1,17 @@
+import { Button } from "react-bootstrap";
 import Header from "../components/Header.js";
-import ResumePdf from "../images/resume/OngNataliaResume2021.png";
+import ResumePdf from "../images/resume/OngNataliaResume2021.pdf";
+import ResumePng from "../images/resume/OngNataliaResume2021.png";
 
 function Resume() {
   return (
     <div style={styles.container}>
       <Header title="Resume" />
-      <img style={styles.image} src={ResumePdf} alt="Resume" />
+      <a href={ResumePdf} download>
+        <Button variant="link">Click To Download</Button>
+      </a>
+      <br />
+      <img style={styles.image} src={ResumePng} alt="Resume" />
     </div>
   );
 }
